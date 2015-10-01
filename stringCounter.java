7df9224c66b1ba8140844a7555342e0521eff08c
FrogFlint <james.flint@outlook.com>
2015-10-01 18:31:24 -0400

    Character counter
    counts the characters in a string

---
+++ stringCounter.java
@@ -0,0 +1,7 @@
+
+public class stringCounter {
+	public static String string = "your string here";
+	public static void main(String[] args){
+System.out.println(string + " has " + string.length() + " digits");
+	}
+}
