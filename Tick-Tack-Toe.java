ed470e7dfad71071a66664fbeadd4edca91e9749
FrogFlint <james.flint@outlook.com>
2015-10-08 19:07:50 -0400

    the game

---
+++ Tick-Tack-Toe.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Tick-Tack-Toe {
+	public static String a = "X";
+	public static String b = "O";
+	public static String c = "O";
+	public static String d = "X";
+	public static String e = "O";
+	public static String f = "X";
+	public static String g = "X";
+	public static String h = "X";
+	public static String i = "O";
+	public static String input;
+	public static String turn = "X";
+	public static void main(String args[]){
+		Scanner look = new Scanner(System.in);
+		//for(;;){
+			System.out.println("  " + a + " " + b + " " +  c);
+			System.out.println("  " + d + " " + e + " " +  f);
+			System.out.println("  " + g + " " + h + " " +  i);
+
+
+		//}
+	}
+}
